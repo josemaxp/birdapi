0b1761fc3cc56d46c2faf38938ac901fd5d0068e
José Manuel Marín <josemaxp@gmail.com>
2024-03-02 17:26:43 +0100

    Update

--- code/birpedy/app/src/main/java/com/josemaxp/birdpedy/ui/navigation/AppScreens.kt
+++
@@ -1,7 +0,0 @@
-package com.josemaxp.birdpedy.ui.navigation
-
-sealed class AppScreens(val route: String) {
-    data object MainScreen: AppScreens("mainScreen")
-    data object MapScreen: AppScreens("mapScreen")
-    data object BirdScreen: AppScreens("birdScreen")
-}
